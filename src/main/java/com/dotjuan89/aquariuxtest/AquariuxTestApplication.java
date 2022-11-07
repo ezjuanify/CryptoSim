09f1d8e8a389c64b239c8b45714098b8e80faf7d
dotjuan89 <dotjuan89@gmail.com>
2022-11-08 01:58:24 +0800

    Spring boot initial commit

---
+++ src/main/java/com/dotjuan89/aquariuxtest/AquariuxTestApplication.java
@@ -0,0 +1,14 @@
+package com.dotjuan89.aquariuxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class AquariuxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariuxTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotjuan89/aquariuxtest/AquariuxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotjuan89.aquariuxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariuxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
